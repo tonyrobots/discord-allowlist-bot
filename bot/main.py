f8370cffd6bca7f7037c66f3664134c0609a74e9
Tony Zito <178870+tonyrobots@users.noreply.github.com>
2021-10-23 15:16:55 -0700

    added todo list. some work towards better accomodating different servers

--- bot/main.py
+++ bot/main.py
@@ -22,11 +22,10 @@ else:
 
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
+ALLOWED_GUILDS = os.getenv("ALLOWED_DISCORD_GUILDS") # Not yet checked against
 MONGO_URL = os.getenv("MONGO_URL")
 TESTING = True
 
-PROJECT_NAME = "CF Test"
 ALLOWED_ROLES = ["Blerxers", "Friends"]  # Wonder how to set via a config UI
 
 cluster = MongoClient(MONGO_URL)
@@ -36,18 +35,21 @@ if TESTING:
 else:
     collection = db["AllowList_prod"]
 
+# project_name = "CF Test"
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await bot.change_presence(activity=discord.Game('Accepting wallet addresses'))
-    for guild in bot.guilds:
-        if guild.name == GUILD:
-            break
+    f'{bot.user} is connected to the following server(s):\n'
 
-    print(
-        f'{bot.user} is connected to the following server:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    for guild in bot.guilds:
+        print (guild.name)
+        # if guild.name == GUILD:
+        #     break
+        print(
+            f'{guild.name}(id: {guild.id})'
+        )
 
 
 ### command listeners
@@ -68,7 +70,7 @@ async def allow(message, arg):
         approved_role = check_eligibility(message.author)
 
         # print(f'writing to db ' + message.author.name + ' as role ' + approved_role)
-        result_message = add_to_list(message.author, approved_role, wallet)
+        result_message = add_to_list(message.author, approved_role, wallet, message.guild.name)
         # await message.channel.send('Hello, ' + message.author.name +'!' + ' You are added to the ' + approved_role + ' list.')
         await message.channel.send(f"Hello, {message.author.name}! {result_message}")
 
@@ -83,10 +85,10 @@ async def check(message):
         return
 
     my_list = check_eligibility(message.author)
-    if user_not_in_list(message.author, my_list):
+    if user_not_in_list(message.author, my_list, message.guild.name):
         await message.channel.send(f"Hello, {message.author.name}! Sorry, you don't appear to be on the '{my_list}' list. Use !allow <wallet address> to add yourself.")
     else:
-        list_entry = get_list_entry(message.author)
+        list_entry = get_list_entry(message)
         await message.channel.send(f'Hi, {message.author.name}! You are in list "{list_entry["listname"]}" with wallet {list_entry["wallet"]}')
 
 # listen for !roles command
@@ -101,11 +103,12 @@ async def roles(message):
 @bot.command(brief='!count to see current allowlist count', cog_name='General')
 @commands.has_permissions(manage_guild=True) # must have manage_guild (server) perms to do count command
 async def count(message, arg=""):
-    doc_count = collection.count_documents({"project": PROJECT_NAME})
+    project_name = message.guild.name #should check to see if in allowed guilds
+    doc_count = collection.count_documents({"project": project_name})
     respString = (f"There are currently {doc_count} addresses on the allowlist.\n")
     if arg=="v" or arg=="verbose":
         for role in ALLOWED_ROLES:
-            role_count = collection.count_documents({"project": PROJECT_NAME, "listname": role})
+            role_count = collection.count_documents({"project": project_name, "listname": role})
             respString += f"{role}: {role_count}\n"
 
     await message.channel.send(respString)
@@ -129,28 +132,32 @@ def check_eligibility(member): # returns top qualifying role for user
             approved_role = role.name
     return approved_role
 
-def add_to_list(member, list, wallet):
-    list_entry = {"project": PROJECT_NAME, "username": member.name, "discordID":member.id, "listname": list, "wallet": wallet, "joinDate": member.joined_at, "currentDate": datetime.datetime.utcnow()}
-    if user_not_in_list(member, list):
+def add_to_list(member, list, wallet, project):
+    project_name = project  # should check to see if in allowed guilds
+
+    list_entry = {"project": project_name, "username": member.name, "discordID":member.id, "listname": list, "wallet": wallet, "joinDate": member.joined_at, "currentDate": datetime.datetime.utcnow()}
+    if user_not_in_list(member, list, project_name):
         # add user to list
         collection.insert_one(list_entry)
         return f'You are added to the {list} list with wallet: {wallet}.'    
     else:
         # user already exists, update previous record instead of adding
-        old_record = collection.find_one_and_update({"project": PROJECT_NAME, "discordID": member.id, "listname": list}, {"$set": {"wallet": wallet}})
+        old_record = collection.find_one_and_update({"project": project_name, "discordID": member.id, "listname": list}, {"$set": {"wallet": wallet}})
         if old_record['wallet'] == wallet:
             return f'You were already on the "{list}" list with that wallet address, so nothing has changed.'
         else:
             return f'You were already on the "{list}" list, but your record has been updated with the new wallet info: {wallet}'
 
-def get_list_entry(member):
-        myquery = { "discordID": member.id, "project": PROJECT_NAME, "listname":check_eligibility(member)  }
+def get_list_entry(message):
+        project_name = message.guild.name  # should check to see if in allowed guilds
+        myquery = { "discordID": message.author.id, "project": project_name, "listname":check_eligibility(message.author)  }
         list_entry = collection.find_one(myquery)
         return list_entry
 
 
-def user_not_in_list(member,list):
-    myquery = { "discordID": member.id, "listname": list, "project": PROJECT_NAME  } # only checking discord ID, should check id, list, project
+def user_not_in_list(member, list, project):
+    project_name = project  # should check to see if in allowed guilds
+    myquery = { "discordID": member.id, "listname": list, "project": project_name  } # only checking discord ID, should check id, list, project
     # print (f"found {collection.count_documents(myquery)} docs that match your discord ID {member.id}")
     return (collection.count_documents(myquery) == 0)
 
