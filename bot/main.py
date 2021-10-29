46b9a3b5f4654ad387baf2bd8a546d11328fdfbe
Tony Zito <178870+tonyrobots@users.noreply.github.com>
2021-10-29 08:08:07 -0700

    added slot machine functionality, and changed to use replies

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,9 @@ from discord.ext import commands
 import discord
 import datetime
 import re
+import random
 from discord.permissions import Permissions
+from discord.utils import get
 import pymongo
 from pymongo import MongoClient
 
@@ -26,8 +28,10 @@ bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 ALLOWED_GUILDS = os.getenv("ALLOWED_DISCORD_GUILDS") # Not yet checked against
 MONGO_URL = os.getenv("MONGO_URL")
+ALLOWED_CHANNELS_SLOTS = ["slot-machine"] #only used by slotmachine
 
-ALLOWED_ROLES = ["Legendary Adventurer", "Epic Explorer", "Rare Seeker", "Uncommon Wanderer", "Friends", "Blerxers"]  # Wonder how to set via a config UI
+
+ALLOWED_ROLES = ["Legendary Adventurer", "Epic Explorer", "Rare Seeker", "Uncommon Wanderer", "Lucky Devil", "Friends", "Blerxers"]  # Wonder how to set via a config UI
 
 cluster = MongoClient(MONGO_URL)
 db = cluster["AllowList"]
@@ -64,7 +68,7 @@ async def allow(message, arg):
     wallet = validate_wallet(arg)
 
     if not wallet:
-        await message.channel.send(f"Sorry, {arg} is not a valid wallet address. (Note: ENS names like 'example.eth' are not supported.)")
+        await message.reply(f"Sorry, {arg} is not a valid wallet address. (Note: ENS names like 'example.eth' are not supported.)")
         return
 
     if check_eligibility(message.author):
@@ -73,11 +77,11 @@ async def allow(message, arg):
         # print(f'writing to db ' + message.author.name + ' as role ' + approved_role)
         result_message = add_to_list(message.author, approved_role, wallet, message.guild.name)
         # await message.channel.send('Hello, ' + message.author.name +'!' + ' You are added to the ' + approved_role + ' list.')
-        await message.channel.send(f"Hey, {message.author.nick or message.author.name}! {result_message}")
+        await message.reply(f"Hey, {message.author.nick or message.author.name}! {result_message}")
 
     else:
-        print('not an approved role!')
-        await message.channel.send(f"Sorry, {message.author.nick or message.author.name}! You don't seem to have a role eligible for the allow list. Try !roles to see eligible roles.")
+        # print('not an approved role!')
+        await message.reply(f"Sorry, {message.author.nick or message.author.name}! You don't seem to have a role eligible for the allow list. Try !roles to see eligible roles.")
 
 # listen for !check command
 @bot.command(brief='!check to check your current list status.', cog_name='General')
@@ -88,14 +92,14 @@ async def check(message):
     my_list = check_eligibility(message.author)
 
     if not my_list:
-        await message.channel.send(f"Sorry, {message.author.nick or message.author.name}. You don't seem to have a role eligible for the allow list. Try !roles to see eligible roles.")
+        await message.reply(f"Sorry, {message.author.nick or message.author.name}. You don't seem to have a role eligible for the allow list. Try !roles to see eligible roles.")
         return
 
     if user_not_in_list(message.author, my_list, message.guild.name):
-        await message.channel.send(f"Hello, {message.author.nick or message.author.name}! You are eligible for the '{my_list}' list, but haven't added your wallet address yet. Use !allow <wallet address> to add yourself.")
+        await message.reply(f"Hello, {message.author.nick or message.author.name}! You are eligible for the '{my_list}' list, but haven't added your wallet address yet. Use !allow <wallet address> to add yourself.")
     else:
         list_entry = get_list_entry(message)
-        await message.channel.send(f'Hi, {message.author.nick or message.author.name}! You are in list "{list_entry["listname"]}" with wallet {list_entry["wallet"]}')
+        await message.reply(f'Hi, {message.author.nick or message.author.name}! You are in list "{list_entry["listname"]}" with wallet {list_entry["wallet"]}')
 
 # listen for !roles command
 @bot.command(brief='!roles to see allow-listed Discord roles', cog_name='General')
@@ -120,7 +124,40 @@ async def count(message, arg=""):
 
     await message.channel.send(respString)
 
-        
+#### Slot Machine stuff, needs some clean-up
+# SLOT_WIN = ":LMaps_crown:"
+# SLOT_LOSS = [":LMaps_wiz_sad:", ":Lmaps_wiz_mad:"]
+
+SLOT_WIN = ":crossed_swords:"
+SLOT_LOSS = [":dizzy_face:",":face_with_spiral_eyes:", ":frowning2:"]
+slot_result = ["","",""]
+
+# listen for !slot command
+@bot.command(brief='!slot to try your luck (only in the slot-machine channel)')
+async def slot(message):
+    # exit command if not the desired channel.
+    if ALLOWED_CHANNELS_SLOTS and message.channel.name not in ALLOWED_CHANNELS_SLOTS:
+        await message.reply("You can only do that in the following channels: " + ','.join(ALLOWED_CHANNELS_SLOTS))
+        return
+
+    if slot_win(message):
+        await message.reply(SLOT_WIN + SLOT_WIN + SLOT_WIN + " -- Winner Winner Chicken Dinner!")
+        # set member role to Lucky Devil
+        winner_role = get(message.guild.roles, name="Lucky Devil")
+        await message.author.add_roles(winner_role)
+        await message.reply(f"You were added to the {winner_role} role, and can now add yourself to the allow list." )
+    else:
+        wins = random.randint(0,2)
+        for i in range(3):
+            if (random.random() < (wins/3) ):
+                slot_result[i] = SLOT_WIN
+                wins -= 1
+            else:
+                slot_result[i] = random.choice(SLOT_LOSS)
+        await message.reply(slot_result[0] + slot_result[1] + slot_result[2] + " -- Sorry, you didn't win.")
+
+        # await message.reply(SLOT_LOSS[0] + SLOT_LOSS[1] + SLOT_WIN)
+
 
 # # listen for !export command
 # @bot.command(brief='!export current allowlist as CSV', cog_name='Admin')
@@ -177,7 +214,11 @@ def get_eligible_guild_roles(guild):
 
 # def export_csv(query='{"project": PROJECT_NAME}'):
 
-    
+def slot_win(message):
+    if random.random() < .3:
+        return True
+    else:
+        return False
 
 def validate_wallet(wallet = ""):
     p = re.compile("0x[a-fA-F0-9]{40}")
