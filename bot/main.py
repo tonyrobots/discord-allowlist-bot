3c481294837fc8b2667ca09eaa1f17a87c06292b
Tony Zito <178870+tonyrobots@users.noreply.github.com>
2021-10-31 18:35:08 -0700

    first version of oracle running

--- bot/main.py
+++ bot/main.py
@@ -14,39 +14,66 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+base_dir = os.path.dirname(__file__)
+
+# What do I have to do to get these emoji to show up??!?
+SLOT_WIN = '<:LMaps_crown:902296702264950805>'
+SLOT_LOSS = ["<:LMaps_wiz_sad:902296702164287489>",
+             "<:LMaps_shield:902296702327877653>",
+             "<:LMaps_torch:902296702504034324>",
+             "<:LMaps_skel:902296702357229569>",
+             "<:LMaps_swords:902296702470463598>",
+             "<:LMaps_potion:902296702424350741>"]
+
 
 if not os.getenv("env") == "dev":
     # PRODUCTION Settings
     os.environ['http_proxy'] = os.environ.get('FIXIE_URL', '')
     os.environ['https_proxy'] = os.environ.get('FIXIE_URL', '')
     TESTING = False
-    SLOT_CHANCE = 1/100
-    # SLOT_WIN = ":LMaps_crown:"
-    # SLOT_LOSS = [":LMaps_wiz_sad:", ":Lmaps_wiz_mad:",
-    #              ":Lmaps_torch:", ":Lmaps_skel:", ":Lmaps_swords:", ":Lmaps_potion:"]
-    SLOT_WIN = ":crossed_swords:"
-    SLOT_LOSS = [":dizzy_face:", ":face_with_spiral_eyes:",
-                 ":frowning2:", ":skull:"]
+    SLOT_CHANCE = 1/40
+
+    # SLOT_WIN = ":crossed_swords:"
+    # SLOT_LOSS = [":dizzy_face:", ":face_with_spiral_eyes:",
+    #              ":frowning2:", ":skull:"]
 
 else:
     # DEV Settings
     from dotenv import load_dotenv
     load_dotenv() 
     TESTING = True
-    SLOT_CHANCE = 75/100
-    SLOT_WIN = ":crossed_swords:"
-    SLOT_LOSS = [":dizzy_face:", ":face_with_spiral_eyes:", ":frowning2:", ":skull:"]
+    SLOT_CHANCE = 20/100
+    # SLOT_WIN = ":crossed_swords:"
+    # SLOT_LOSS = [":dizzy_face:", ":face_with_spiral_eyes:", ":frowning2:", ":skull:"]
 
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 ALLOWED_GUILDS = os.getenv("ALLOWED_DISCORD_GUILDS") # Not yet checked against
 MONGO_URL = os.getenv("MONGO_URL")
-ALLOWED_CHANNELS_SLOTS = ["slot-machine",
-                          "🎰│slot-machine","admin-test-channel"]  # only used by slotmachine
+ALLOWED_CHANNELS_SLOTS = ["oracle", "admin-test-channel"]  # only used by slotmachine
 ALLOWED_CHANNELS_ALLOWLISTER = ["whitelist", "whitelist_private_booth",
                                 "allowlist", "bots", "📝│whitelist","admin-test-channel"]  # only used by allow lister, not !slot
+ENABLE_SLOT = False
+ENABLE_ORACLE = True
+if ENABLE_ORACLE:
+    # Load oracle strings
+    my_file = open(os.path.join(base_dir, "oracle_messages.txt"), "r")
+    ORACLE_MESSAGES = my_file.readlines()
+    my_file.close()
+    print(f"Loaded {len(ORACLE_MESSAGES)} oracle messages from file.")
 
 
+# SLOT_WIN = discord.utils.get(bot.emojis, name="LMaps_crown")
+# SLOT_LOSS = [
+#     discord.utils.get(bot.emojis, name="LMaps_wiz_sad"),
+#     discord.utils.get(bot.emojis, name="LMaps_wiz_angry"),
+#     discord.utils.get(bot.emojis, name="LMaps_shield"),
+#     discord.utils.get(bot.emojis, name="LMaps_torch"),
+#     discord.utils.get(bot.emojis, name="LMaps_potion"),
+#     discord.utils.get(bot.emojis, name="LMaps_skel"),
+# ]
+
+# print (bot.emojis)
 
 ALLOWED_ROLES = ["Legendary Adventurer", "Epic Explorer", "Rare Seeker", "Uncommon Wanderer", "Friends", "Blerxers"]  # Wonder how to set via a config UI
 
@@ -72,6 +99,8 @@ async def on_ready():
         print(
             f'{guild.name}(id: {guild.id})'
         )
+        await guild.me.edit(nick="Legend Maps Oracle")
+
 
 
 ### command listeners
@@ -160,52 +189,99 @@ async def count(message, arg=""):
 async def info(message):
     await message.channel.send(f"`slot odds: {SLOT_CHANCE} `")
 
-#### Slot Machine stuff, needs some clean-up
-slot_result = ["","",""]
 
-# listen for !slot command
-@bot.command(brief='!slot to try your luck (only in the slot-machine channel)')
-async def slot(message):
-    # exit command if not an allowed channel.
-    #only allow in defined channel(s)
-    if not is_allowed_channel(message, ALLOWED_CHANNELS_SLOTS):
-        await wrong_channel_message(message, ALLOWED_CHANNELS_SLOTS)
-        return
+if ENABLE_ORACLE:
+    slot_result = ["", "", ""]
+    # listen for !sacrifice command
+    @bot.command(brief='make a !sacrifice to the oracle, and she may reward you with wealth or wisdom')
+    async def sacrifice(message):
+        # exit command if not an allowed channel.
+        #only allow in defined channel(s)
+        if not is_allowed_channel(message, ALLOWED_CHANNELS_SLOTS):
+            await wrong_channel_message(message, ALLOWED_CHANNELS_SLOTS)
+            return
+        # do the regular sacrifice oracle message thing
+        # await message.reply(f"You make your sacrifice to the Oracle, and a disembodied voice emerges from the shadows to say:\n `{random.choice(ORACLE_MESSAGES)}`")    
+        oracle_reply = f"You complete your sacrifice, and a swirling vision forms before you...\n"
 
-    if slot_win(message):
-        await message.reply(f"{SLOT_WIN}  {SLOT_WIN}  {SLOT_WIN} -- Winner Winner Chicken Dinner!")
-        # set member role to Lucky Devil
+        if slot_win(message):
+            # await message.reply(f"Suddenly, you hear a great and distant rumbling, and a searing light fills your eyes. You heart fills with radiance -- you have been *blessed* by the gods!")
+            oracle_reply += f"{SLOT_WIN}   {SLOT_WIN}   {SLOT_WIN}\n"
+            # oracle_reply += f"`{random.choice(ORACLE_MESSAGES)}`"
+            oracle_reply += f"Suddenly, you hear a great and distant rumbling, and a searing light fills your eyes. A voice from the shadows booms `YOU HAVE BEEN CHOSEN FOR MY BLESSING.`\n"
+            
+            # set member role to Blessed
+            # TODO clean this up!
+            winner_role1 = get(message.guild.roles, name="Blessed")
 
-        # TODO clean this up!
-        winner_role1 = get(message.guild.roles, name="Lucky Devil")
-
-        # only give uncommon wanderer if they don't have a higher role. Really gotta generalize this!
-        if check_eligibility(message.author):
-            await message.reply(f"You already had an eligible role, so we'll just call you a lucky devil :)")
-            await message.author.add_roles(winner_role1)
-        else:
-            winner_role2 = get(message.guild.roles, name="Uncommon Wanderer")
-            await message.author.add_roles(winner_role1, winner_role2)
-
-            await message.reply(f"You are now a {winner_role2}, and can now add yourself in the whitelist channel with command !allow <wallet address>.")
-
-    else:
-        wins = random.randint(0,2)
-        for i in range(3):
-            if (random.random() < (wins/3) ):
-                slot_result[i] = SLOT_WIN
-                wins -= 1
+            # only give uncommon wanderer if they don't have a higher role. Really gotta generalize this!
+            if check_eligibility(message.author):
+                oracle_reply += (
+                    f"You already had an eligible role, so we'll just call you 'blessed.' ")
+                await message.author.add_roles(winner_role1)
             else:
+                winner_role2 = get(message.guild.roles,
+                                   name="Uncommon Wanderer")
+                await message.author.add_roles(winner_role1, winner_role2)
+
+                oracle_reply += (
+                    f"You are now a {winner_role2}, and can now add yourself in the whitelist channel with command !allow <wallet address>.")
+        else:
+            # output loser images
+            for i in range(2):
                 slot_result[i] = random.choice(SLOT_LOSS)
-        await message.reply(f"{slot_result[0]}  {slot_result[1]}  {slot_result[2]}  -- Sorry, you didn't win.")
+            oracle_reply += f"{slot_result[0]}   {slot_result[1]}\n"
+            oracle_reply += f"As you puzzle over its meaning, a disembodied voice emerges from the shadows to say:\n `{random.choice(ORACLE_MESSAGES)}`"
+        
+        await message.reply(oracle_reply)
+
+if ENABLE_SLOT: 
+    #### Slot Machine stuff, needs some clean-up
+    slot_result = ["","",""]
+
+    # listen for !slot command
+    @bot.command(brief='!slot to try your luck (only in the slot-machine channel)')
+    async def slot(message):
+        # exit command if not an allowed channel.
+        #only allow in defined channel(s)
+        if not is_allowed_channel(message, ALLOWED_CHANNELS_SLOTS):
+            await wrong_channel_message(message, ALLOWED_CHANNELS_SLOTS)
+            return
+
+        if slot_win(message):
+            await message.reply(f"{SLOT_WIN}  {SLOT_WIN}  {SLOT_WIN} -- Winner Winner Chicken Dinner!")
+            # set member role to Lucky Devil
+
+            # TODO clean this up!
+            winner_role1 = get(message.guild.roles, name="Lucky Devil")
+
+            # only give uncommon wanderer if they don't have a higher role. Really gotta generalize this!
+            if check_eligibility(message.author):
+                await message.reply(f"You already had an eligible role, so we'll just call you a lucky devil :)")
+                await message.author.add_roles(winner_role1)
+            else:
+                winner_role2 = get(message.guild.roles, name="Uncommon Wanderer")
+                await message.author.add_roles(winner_role1, winner_role2)
+
+                await message.reply(f"You are now a {winner_role2}, and can now add yourself in the whitelist channel with command !allow <wallet address>.")
+
+        else:
+            wins = random.randint(0,2)
+            for i in range(3):
+                if (random.random() < (wins/3) ):
+                    slot_result[i] = SLOT_WIN
+                    wins -= 1
+                else:
+                    slot_result[i] = random.choice(SLOT_LOSS)
+            await message.reply(f"{slot_result[0]}  {slot_result[1]}  {slot_result[2]}  -- Sorry, you didn't win.")
 
 
-# # listen for !export command
-# @bot.command(brief='!export current allowlist as CSV', cog_name='Admin')
-# @commands.has_permissions(manage_guild=True)
-# async def export(message):
-#     doc_count = collection.count_documents({"project": PROJECT_NAME})
-#     await message.channel.send(f"There are currently {doc_count} addresses on the allowlist.")
+    # # listen for !export command
+    # @bot.command(brief='!export current allowlist as CSV', cog_name='Admin')
+    # @commands.has_permissions(manage_guild=True)
+    # async def export(message):
+    #     doc_count = collection.count_documents({"project": PROJECT_NAME})
+    #     await message.channel.send(f"There are currently {doc_count} addresses on the allowlist.")
 
 
 #### helper functions
@@ -292,6 +368,8 @@ async def wrong_channel_message(message,allowed_channels):
     # filtered_allowed_channels = filter_channels(message, allowed_channels)
     # await message.reply("You can only do that in the following channels: " + ', '.join(filtered_allowed_channels))
     await message.reply("Sorry, you can't do that in this channel.")
+
+
 
 
 # def user_is_admin(member):
